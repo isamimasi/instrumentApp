8c058488f433d99d25917b889b77b958bf20b226
isamimasi <41987332+isamimasi@users.noreply.github.com>
2024-05-08 18:54:25 +0900

    Initial Commit

---
+++ instrumentApp/instrumentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  instrumentAppApp.swift
+//  instrumentApp
+//
+//  Created by F. ISAMI. on 2024/05/08.
+//
+
+import SwiftUI
+
+@main
+struct instrumentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
